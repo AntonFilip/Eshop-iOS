f21db679a1c6f700efe2d0729970ca6756a0a437
Duje Medak <duje.medak@fer.hr>
2018-07-05 23:28:50 +0200

    add profile view layout

--- Shop/Controllers/ProfileViewController.swift
+++ Shop/Controllers/ProfileViewController.swift
@@ -13,13 +13,5 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
 }
