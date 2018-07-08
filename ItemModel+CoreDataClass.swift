a77afdf74ffde8ee73fa8b6ac74e2d7655894faa
Lovro Buničić <lovro.bunicic@fer.hr>
2018-07-08 11:03:25 +0200

    Core data bad predicate fixed

--- ItemModel+CoreDataClass.swift
+++ ItemModel+CoreDataClass.swift
@@ -2,7 +2,7 @@
 //  ItemModel+CoreDataClass.swift
 //  
 //
-//  Created by Duje Medak on 30/06/2018.
+//  Created by Lovro Buničić on 08/07/2018.
 //
 //
 
@@ -11,21 +11,21 @@ import CoreData
 
 @objc(ItemModel)
 public class ItemModel: NSManagedObject {
-    
     class func createFrom(json: [String: Any]) -> ItemModel? {
         if  let name = json["name"] as? String,
             let price = json["salePrice"] as? Double,
             let thumbnail = json["thumbnailImage"] as? String,
             let itemDescription = json["shortDescription"] as? String,
             let id = json["itemId"] as? Int32{
-
+            
             let item = ItemModel.firstOrCreate(with: ["id": String(id)])
             item.name = name
             item.price = String(price)
             item.thumbnail = thumbnail
             item.shortItemDescription = itemDescription
+            item.isFavourite = false
             return item
-
+            
         }
         return nil
     }
